a148dce73c4ec0c8d8d8273f433394f7225f1144
Huseyin <huseyinozdogan@gmail.com>
2020-11-19 16:57:37 +0300

    "2nd example done"

---
+++ django_2ndex/proTwo/appTwo/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django import forms
+from django.urls import reverse
+
+# Create your views here.
+
+class NewTaskForm(forms.Form):
+  task = forms.CharField(label='New Task')
+
+def index(request):
+  if 'tasks' not in request.session:
+    request.session['tasks'] = []
+  return render(request,"appTwo/index.html", {
+    'tasks': request.session['tasks']
+  })
+
+def add(request):
+  if request.method == 'POST':
+    form = NewTaskForm(request.POST)
+    if form.is_valid():
+      task = form.cleaned_data['task']
+      request.session['tasks'] += [task]
+      return HttpResponseRedirect(reverse('appTwo:index'))
+    else:
+      return render(request, 'appTwo/add.html', {
+        'form': form
+      })
+  return render(request, 'appTwo/add.html',{
+    'form':NewTaskForm()
+  })
+
